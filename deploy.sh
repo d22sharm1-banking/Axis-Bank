693080c43e20afd921f90b15fc2eae244f6e3b21
d22sharm <ds4udeepakshama@gmil.com>
2023-10-10 06:23:52 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 this file is deploy.sh
+we are ging o fetch/pull this code
 
-helolllllllllllllllllllllllllllll
--- employeeservices.java
+++ employeeservices.java
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 updating in devopment branch
 
+=======
+updating for stash
+>>>>>>> 1857b27 (updated)
 this file contains employee data
